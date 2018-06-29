2ae079e2defc351559023259913d44b0060ddd8e
Savannah Pyle <bea12022@byui.edu>
2018-06-28 21:45:34 -0600

    add proc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.set("port", 5000)
+app.set("port", process.env.PORT || 5000)
     .use(express.json())
     .use(express.urlencoded())
 	.use(express.static(__dirname + "/public"))
